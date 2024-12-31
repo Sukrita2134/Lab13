16b1e44140035e9ffdb2e9831160cb3ae112ed97
Sukrita <sugritabursuk@gmail.com>
2024-12-31 14:12:55 +0700

    lab13_1 completed

--- lab13_1.cpp
+++ lab13_1.cpp
@@ -19,3 +19,31 @@ int main(){
     cout << "Min = " << B[5];
     return 0;
 }
+void stat(const double A[], int N, double B[]) {
+    double sum = 0.0;
+    for (int i = 0; i<N; ++i) {
+        sum += A[i];
+    }
+    B[0] = sum / N;
+    double variance = 0.0;
+    for (int i = 0; i < N; ++i) {
+        variance += pow(A[i] - B[0], 2);
+    }
+    B[1] = sqrt(variance / N);
+    double product = 1.0;
+    for (int i = 0; i < N; ++i) {
+        product *= A[i];
+    }
+    B[2] = pow(product, 1.0 / N);
+    double harmonicSum = 0.0;
+    for (int i = 0; i < N; ++i) {
+        harmonicSum += 1.0 / A[i];
+    }
+    B[3] = N / harmonicSum;
+    B[4] = A[0];
+    B[5] = A[0];
+    for (int i = 1; i < N; ++i) {
+        if (A[i] > B[4]) B[4] = A[i];
+        if (A[i] < B[5]) B[5] = A[i];
+    }
+}
